72fe544933e887953d2a3b6521fccbdd4d824907
developer <compuktermal@outlook.com>
2026-04-14 22:36:23 +0100

    Learning C, day 6

--- Week-1/Day-6/6.c
+++ Week-1/Day-6/6.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 
-struct string_stats {
-    int digits;
-    int letters;
-    int spaces;
-    size_t length;
-    int other;
-};
-
 struct array_stats {
-    int ints;
-    int strings;
-
     long long int_sum;
 
     int letters;
     int digits;
     int spaces;
+
+    int ints;
+    int strings;
+
     int others;
 };
 
@@ -34,12 +27,6 @@ int is_digit(const char data) {
     return 0;
 }
 
-void swap_integers(int *a, int *b) {
-    *a = *a + *b;
-    *b = *a - *b;
-    *a = *a - *b;
-}
-
 int is_integer(const char *data, size_t data_size) {
     for (size_t i = 0; i < data_size && (data[i] != ' ' || data[i] != '\n' || data[i] != '\0'); ++i) {
         if (!is_digit(data[i])) {
@@ -49,27 +36,41 @@ int is_integer(const char *data, size_t data_size) {
     return 1;
 }
 
-void print_array_stats(struct string_stats array_stats) {
+int is_string(const char *data, size_t data_size) {
+    for (size_t i = 0; i < data_size && (data[i] != ' ' || data[i] != '\n' || data[i] != '\0'); ++i) {
+        if (!is_alpha(data[i])) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void swap_integers(int *a, int *b) {
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+}
+
+void print_array_stats(struct array_stats array_stats) {
     printf("Array stats: \n");
     printf("Letters: %d\n", array_stats.letters);
     printf("Digits: %d\n", array_stats.digits);
     printf("Spaces: %d\n", array_stats.spaces);
-    printf("Other data: %d\n", array_stats.other);
-    printf("Total array size (in bytes): %zu\n", array_stats.length);
+    printf("Strings: %d\n", array_stats.strings);
+    printf("Integers: %d\n", array_stats.ints);
+    printf("Sum of all integers: %lld\n", array_stats.int_sum);
+    printf("Other data: %d\n", array_stats.others);
 }
 
 
-struct string_stats analyze_array_data(const char *array, size_t array_size) {
-    struct string_stats array_stats = {0, 0, 0, array_size, 0};
+struct array_stats analyze_array(const char *array, size_t array_size) {
+    struct array_stats array_stats = {0, 0, 0, 0, 0, 0, 0};
+
     const char *array_pointer = array;
-    const char *array_end = array + array_size;
+    int temp_int = 0;
 
-    while (array_pointer < array_end) {
-        if (*array_pointer == ' ') {
-            array_stats.spaces++;
-        }
-
-        else if (is_alpha(*array_pointer)) {
+    while (array_pointer < array + array_size) {
+        if (is_alpha(*array_pointer)) {
             array_stats.letters++;
         }
 
@@ -77,8 +78,25 @@ struct string_stats analyze_array_data(const char *array, size_t array_size) {
             array_stats.digits++;
         }
 
+        else if (*array_pointer == ' ') {
+            array_stats.spaces++;
+        }
+
         else {
-            array_stats.other++;
+            array_stats.others++;
+        }
+
+        if (is_integer(array_pointer, array_size)) {
+            for (const char *pointer = array_pointer; pointer < array + array_size && (*pointer != ' ' || *pointer != '\n' || *pointer != '\0'); ++pointer) {
+                temp_int = temp_int * 10 + *pointer;
+            }
+
+            array_stats.ints++;
+            array_stats.int_sum += temp_int;
+        }
+
+        else if (is_string(array_pointer, array_size)) {
+            array_stats.strings++;
         }
 
         array_pointer++;
@@ -87,26 +105,24 @@ struct string_stats analyze_array_data(const char *array, size_t array_size) {
     return array_stats;
 }
 
-int analyze_array_advanced(int *array, size_t array_size) {
-    return 0;
-}
-
 int main(void) {
+    struct array_stats array_stats = {0, 0, 0, 0, 0, 0, 0};
     char buffer[256];
-    size_t data_size = 0;
-    struct string_stats array_stats = {0, 0, 0, 0, 0};
+
+    size_t buffer_size = 0;
 
     printf("Hello, enter any data: ");
-
 
     if (!fgets(buffer, sizeof(buffer), stdin)) {
         printf("Error writing to buffer\n");
         return 1;
     }
 
-    for (; data_size < sizeof(buffer) && buffer[data_size] != '\0'; ++data_size);
-
-    array_stats = analyze_array_data(buffer, data_size);
+    while (buffer[buffer_size] != '\0') {
+        buffer_size++;
+    }
+    
+    array_stats = analyze_array(buffer, buffer_size);
     print_array_stats(array_stats);
 
     return 0;
