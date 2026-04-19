c30c2baa2bba0aa19aa6bdeffd790e3555014242
developer <compuktermal@outlook.com>
2026-04-19 21:22:21 +0100

    Learning C, day 12

--- Week-2/Day-5/something.c
+++ Week-2/Day-5/something.c
@@ -21,7 +21,7 @@ char* copy_str(char* source) {
     return copied_list;
 }
 
-int main (void) {
+int copy_string_examlle (void) {
     char buffer[256];
 
     printf("Hello, enter some data: ");
@@ -42,4 +42,61 @@ int main (void) {
     fprintf(stdout, "[Info] Copied string's memory address in heap: %p\n", dest);
     fprintf(stdout, "[Info] Freeing allocated memory...\n");
     free(dest);
+
+    return 0;
+}
+
+int dynamical_float_array(void){
+    size_t array_elements = 5;
+    size_t counter = 0;
+    float* array = (float*)malloc(sizeof(float) * array_elements);
+    char buffer[256];
+
+    fprintf(stdout, "Hello, this program creates a dynamic list of floats. Enter any float, if the list becomes full, it will be resized\n");
+    if (array == NULL) {
+        fprintf(stderr, "[Error] Error allocating %zu bytes list for floats\n", sizeof(float) * array_elements);
+        return -2;
+    }
+    fprintf(stdout, "[Info] Allocated %zu bytes memory block for %zu floats\n", sizeof(float) * array_elements, array_elements);
+
+    while (1) {
+        if (counter >= array_elements) {
+            fprintf(stdout, "[Info] Array size limit reached. Resizing array to fit 10 more floats...\n");
+            array_elements += 10;
+
+            float* tmp = (float*)realloc(array, sizeof(float) * array_elements);
+
+            if (tmp == NULL) {
+                fprintf(stderr, "[Error] Error allocating %zu bytes list for floats\n", sizeof(float) * array_elements);
+                free (array);
+                return -2;
+            }
+
+            array = tmp;
+        }
+
+        fprintf(stdout, "Your float: ");
+        
+        if (!fgets(buffer, sizeof(buffer), stdin) || sscanf(buffer, "%f", array + counter) != 1) {
+            fprintf(stdout, "[Warning] Incorrect input. Enter only float data or 0 to exit\n");
+            continue;
+        }
+        if (*(array + counter) == 0.0f) {
+            fprintf(stdout, "[Info] 0 detected, exiting...\n");
+            break;
+        }
+        counter++;
+    }
+
+    fprintf(stdout, "You have entered %zu floats. Allocated memory's size is %zu bytes\n", counter, sizeof(float) * array_elements);
+    fprintf(stdout, "[Info] Freeing %zu bytes allocated memory...\n", sizeof(float) * array_elements);
+    free(array);
+
+    return 0;
+}
+
+int main(void) {
+    dynamical_float_array();
+
+    return 0;
 }
