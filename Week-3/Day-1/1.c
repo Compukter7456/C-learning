b2524f27db9d2362fb5eca097fc8130a708eaab0
developer <compuktermal@outlook.com>
2026-04-22 18:13:39 +0100

    Learning C, day 14

--- Week-3/Day-1/1.c
+++ Week-3/Day-1/1.c
@@ -1,45 +1,64 @@
 #include <stdio.h>
-#include <unistd.h> // sleep
+#include <unistd.h> // sleep()
 
-#define WORKINGFILE "test.txt"
-#define BUFFERSIZE 32
+#define WORKING_FILE "test.txt"
+#define BUFFER_SIZE 32
 
-int file_descriptors(void) {
-    
-    // FILE is glibc struct which contains info about file with which we are working
-    FILE *file = fopen(WORKINGFILE, "w"); // file1 contains memory address of struct FILE that works with test1.txt
-    // /proc/PID/fd/3 is file descriptor of test1.txt
-    // If we delete/unlink file (but program has open fd) - file will be accessible via that fd
+int create_file(void) {
+    FILE *file = fopen(WORKING_FILE, "w");
 
     if (file == NULL) {
         fprintf(stderr, "[Error] Cannot open file\n");
         return -1;
     }
 
-    // fprintf allows to write data into set stream, like stdin, stdout, stderr, files, etc
-    // printf("Hello\n"); is fprintf(stdout, "Hello\n");
-    fprintf(stdout, "[Info] Writing \"Hello from C\" to %s. Address of FILE: %p\n", WORKINGFILE, file);
-    fprintf(file, "NAME    AGE   CITY\nabc     12    Chicago\ndef     25    Colego\nghi     65    Cleveland\n");
+    fprintf(stdout, "[Info] Writing data to %s. FILE's address: %p\n", WORKING_FILE, file);
+    fprintf(file, 
+        "NAME    AGE   CITY\n"
+        "abc     12    Chicago\n"
+        "def     25    Colego\n"
+        "ghi     65    Cleveland\n"
+    );
 
     fclose(file);
-
     return 0;
 }
 
 int fscanf_demo(void) {
-    FILE *file = fopen(WORKINGFILE, "r");
+    FILE *file = fopen(WORKING_FILE, "r");
 
     if (file == NULL) {
-        fprintf(stderr, "[Error] There is no such file %s\n", WORKINGFILE);
+        fprintf(stderr, "[Error] There is no such file %s\n", WORKING_FILE);
         return -1;
     }
 
-    char a[BUFFERSIZE], b[BUFFERSIZE], c[BUFFERSIZE];
+    char name[BUFFER_SIZE];
+    char age[BUFFER_SIZE];
+    char city[BUFFER_SIZE];
 
-    fprintf(stdout, "[Info] Reading file %s...\n", WORKINGFILE);
+    fprintf(stdout, "[Info] Reading file %s...\n", WORKING_FILE);
 
-    while (fscanf(file, "%31s %31s %31s", a, b, c) == 3) {
-        printf("%s %s %s\n", a, b, c);
+    while (fscanf(file, "%31s %31s %31s", name, age, city) == 3) {
+        printf("%s %s %s\n", name, age, city);
+    }
+
+    fclose(file);
+    return 0;
+}
+
+int fgets_demo(void) {
+    FILE *file = fopen(WORKING_FILE, "r");
+
+    if (file == NULL) {
+        fprintf(stderr, "[Error] There is no such file %s\n", WORKING_FILE);
+        return -1;
+    }
+
+    char buff[BUFFER_SIZE];
+
+    for (size_t i = 0; fgets(buff, sizeof(buff), file) != NULL; i++) {
+        sleep(1);
+        fprintf(stdout, "%zu's line in file: %s", i+1, buff);
     }
 
     fclose(file);
@@ -47,7 +66,8 @@ int fscanf_demo(void) {
 }
 
 int main(void) {
-    file_descriptors();
+    create_file();
     fscanf_demo();
+    fgets_demo();
     return 0;
 }
